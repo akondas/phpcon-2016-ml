2e4a8acc3c30bd9a5d73c87ab5e95590efc860d2
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-09-27 23:31:56 +0200

    First exampels

--- examples/classification.php
+++ examples/classification.php
@@ -1,2 +1,20 @@
 <?php
 
+namespace Machine;
+
+require 'vendor/autoload.php';
+
+use Phpml\Classification\KNearestNeighbors;
+use Phpml\CrossValidation\RandomSplit;
+use Phpml\Dataset\Demo\Iris;
+use Phpml\Metric\Accuracy;
+
+$dataset = new Iris();
+$split = new RandomSplit($dataset);
+
+$classifier = new KNearestNeighbors(2);
+$classifier->train($split->getTrainSamples(), $split->getTrainLabels());
+
+$predicted = $classifier->predict($split->getTestSamples());
+
+echo sprintf("Accuracy: %s", Accuracy::score($split->getTestLabels(), $predicted));
---
+++ examples/regression.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Machine;
+
+use Phpml\Regression\LeastSquares;
+
+require 'vendor/autoload.php';
+
+$samples = [[9300], [10565], [15000], [15000], [17764], [57000], [65940], [73676], [77006], [93739], [146088], [153260]];
+$targets = [7100, 15500, 4400, 4400, 5900, 4600, 8800, 2000, 2750, 2550,  960, 1025];
+
+$regression = new LeastSquares();
+$regression->train($samples, $targets);
+
+
